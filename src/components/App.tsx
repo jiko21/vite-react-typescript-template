1f49a49de8f218287ea0cab22a96aad8806bfd1f
jiko21 <daikikojima0531@icloud.com>
2020-09-19 13:59:48 +0900

    Initialize commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC<{}> = () => (
+  <React.StrictMode>
+    <h1>Hello World</h1>
+  </React.StrictMode>
+);
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+const reactPlugin = require('vite-plugin-react');
+
+module.exports = {
+  jsx: 'react',
+  plugins: [require('vite-plugin-react')],
+};
